a0edbe4a1dfc948ef3fff894dc7404983aa98249
Biswarup Ghoshal <biswarup.ghoshal@gmail.com>
2020-02-25 14:24:22 +0100

    Test..

--- Test.php
+++ Test.php
@@ -6,29 +6,11 @@ include_once './classes/prescription_header.php';
 $admin = new admin();
 
 
+$resultObj = $adminObj->getUserDetails('2');
 
-/* class Foo {
-	public $aMemberVar = 'aMemberVar Member Variable';
-	public $aFuncName = 'aMemberFunc';
-	
-	
-	function aMemberFunc() {
-		print 'Inside `aMemberFunc()`';
-	}
-}
-
-$foo = new Foo; 
-$element = 'aMemberVar';
-print $foo->$element; */
-
-echo $admin->calcBMI(84, 178);
-echo "ideal body weight ->".$admin->calIdealBodyWeight('Male', 178);
-
-$admin->insertUpdatePatientInvestigation('CREATININE', '', '', '1.2', '4918', '21152','dsanyal','anandaclinic');
-
-
-
- include_once './inc/footer.php';
+echo $resultObj->user_full_name;
+?>
+<?php  include_once './inc/footer.php';
 ?>
 </body>
 </html>
